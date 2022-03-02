134a7af39f87b519ec730549c403315b6677a0b3
Rodrigo Santiago <trumpetista@gmail.com>
2022-03-02 02:18:18 -0300

    teste

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LandiFy'
+copyright = '2022, SystemRaise Tecnologia LTDA'
+author = 'SystemRaise Tecnologia LTDA'
 
 release = '0.1'
 version = '0.1.0'
